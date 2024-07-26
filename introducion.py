093cf3e5037a7280fd9cb7967d864e7185498596
Felipe Leite <felipesd.leite@gmail.com>
2024-07-26 11:17:14 -0300

    feat: adding a variable

--- introducion.py
+++ introducion.py
@@ -4,3 +4,12 @@ print(int(1), type(int('1')))
 print(type(float('1') + 1))
 print(float('1') + 1)
 print(bool(''))
+
+complete_name = 'Felipe Milk Leche'
+age = 25
+weight = 79.5
+
+print("{} tem {} anos e pesa {}kg".format(complete_name, age, weight)) #A way to print that informations above
+
+a_ten_times = 'A' * 10
+print(a_ten_times)
