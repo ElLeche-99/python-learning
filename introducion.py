ca12ca676ce3cc522514b5523e9724e69fbb110a
Felipe Leite <felipesd.leite@gmail.com>
2024-07-26 10:48:26 -0300

    feat: adding types exemples and concatenatio

---
+++ introducion.py
@@ -0,0 +1,6 @@
+#This archive will be used to put introducion conceptions from python development
+
+print(int(1), type(int('1')))
+print(type(float('1') + 1))
+print(float('1') + 1)
+print(bool(''))
